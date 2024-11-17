0f386984f10ecb3a766ab9f4fa797824aab16430
quangtran789 <quangvutran61@gmail.com>
2024-11-17 17:50:20 +0700

    new second commit

--- lib/Views/Home/Screens/home.dart
+++ lib/Views/Home/Screens/home.dart
@@ -379,7 +379,7 @@ class _HomeState extends State<Home> {
                                                       TextOverflow.ellipsis,
                                                 ),
                                                 Text(
-                                                  '${AppLocalizations.of(context)!.get('price')}: ${product.price} VND',
+                                                  '${product.price} VND',
                                                   style: const TextStyle(
                                                       fontSize: 14),
                                                 ),
